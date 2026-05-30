10e176e19642534ab988e0a39e240f72c49cfd59
Thomas Riveros <thomasriveros@MacBook-Air.local>
2026-05-30 14:29:51 -0400

    Initial commit of flat Datawrapper comparative CPI dashboard website

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'production' ? '/robust-cpi-bolivia/' : '/',
+    base: mode === 'production' ? '/robust-bolivia-cpi-website/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
